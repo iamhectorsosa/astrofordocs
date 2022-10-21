452eaf1a837502a3ad994b366d66aac13e2e3d76
Hector Sosa <hectorsosa@ip-10-0-0-32.ec2.internal>
2022-10-21 15:00:19 +0200

    🚧 Adding Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/static";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+    integrations: [tailwind()],
+    output: "server",
+    adapter: vercel(),
 });
